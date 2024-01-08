cc21d857d3f9fa3af9586f209142d76dc40f82cd
boplanet <boplanet@gmail.com>
2024-01-08 22:36:54 +0100

    Add project files.

---
+++ EmployeeListApp/Data/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesListApp.Data
+{
+    public class Employee
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18, 2)")]
+        [Range(typeof(decimal), "0,00", "9999999999999999,99", ErrorMessage = "Plaća može biti do 18 i 2 decimalna mjesta.")]
+        public decimal Salary { get; set; }
+
+    }
+}
